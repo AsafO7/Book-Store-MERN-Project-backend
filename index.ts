cbdd54d48ec5d7164c896a06f4447a28f51fb5f5
AsafO7 <ovasaf@gmail.com>
2024-12-04 19:38:00 +0200

    first commit

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(express.json())
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://book-store-mern-project-chi.vercel.app'],
+  origin: ['http://localhost:5173', 'https://book-store-mern-project-chi.vercel.app', 'https://book-store-mern-project-backend-94fm.vercel.app/'],
   credentials: true,
 }))
 
